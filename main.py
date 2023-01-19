5e0a66009386960c2a22c767e761fd2eb9022abf
Eric Salkovic <salk0002@algonquinlive.com>
2023-01-18 22:49:59 -0500

    Commit 1: End of day 1 programming. can read and print .csv file into console. Created Potato class/objects. Currently working on printing data out, then writing to .csv file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import csv
 from Potatoes import Potatoes
+from pprint import pprint
 
 try:
     with open("32100358.csv", "r") as file:
@@ -7,10 +8,11 @@ try:
         header = [next(csvreader)]
         for row in csvreader:
             print(row)
-    potato1 = Potatoes([0],[1],[2],[3],[4],[5],[6],[7],[8],[9],[10],[11],[12],[13])
+    potato1 = Potatoes(row[0], row[1], row[2], row[3], row[4], row[5], row[6],
+                       row[7], row[8], row[9], row[10], row[11], row[12], row[13])
 except FileNotFoundError:
     print("invalid file name")
 except:
     print("something went wrong...")
 
-print("potato1: " + str(potato1))
+print(str(potato1))
